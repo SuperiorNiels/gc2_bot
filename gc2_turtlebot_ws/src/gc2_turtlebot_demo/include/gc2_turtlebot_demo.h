38a66a62941f378ef296240d19d2e00651af0131
Niels Balemans <niels@ubuntu.localdomain>
2020-10-01 18:48:09 +0200

    added demo

---
+++ gc2_turtlebot_ws/src/gc2_turtlebot_demo/include/gc2_turtlebot_demo.h
@@ -0,0 +1,21 @@
+#ifndef GC2_TURTLEBOT_DEMO
+#define GC2_TURTLEBOT_DEMO
+
+#include <ros/ros.h>
+#include <termios.h>
+#include <kuboki_msgs/KeyboardInput.h>
+
+namespace gc2_demo {
+
+class TurtlebotDemo {
+
+    TurtlebotDemo();
+    ~TurtlebotDemo();
+    bool init();
+
+    void run)();
+}
+
+}
+
+#endif
